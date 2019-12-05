3a4191f8330b551ac92b4a55fc6d8cae481d250c
Calvin Gerling <calvin.gerling@singlebit.org>
2019-12-05 12:51:39 -0300

    test(problem01): add an extra test case

--- solutions/test/ninety_nine_elixir_problems/solutions/problem01_test.exs
+++ solutions/test/ninety_nine_elixir_problems/solutions/problem01_test.exs
@@ -11,5 +11,9 @@ defmodule NinetyNineElixirProblems.Solutions.Problem01Test do
     test "should return the last item on a list" do
       assert Problem01.my_last([:x, :y, :z]) == :z
     end
+
+    test "should return the last item from a list with a single item" do
+      assert Problem01.my_last([:x]) == :x
+    end
   end
 end
