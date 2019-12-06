45a9cb86de327b8e901563afab53f9e967352c4b
Calvin Gerling <calvin.gerling@singlebit.org>
2019-12-05 21:01:13 -0300

    refactor(problem01): make clause match a list with single item
    Match a list with single item instead of matching a list with an empty tail

--- solutions/lib/ninety_nine_elixir_problems/solutions/problem01.ex
+++ solutions/lib/ninety_nine_elixir_problems/solutions/problem01.ex
@@ -1,7 +1,7 @@
 defmodule NinetyNineElixirProblems.Solutions.Problem01 do
   def my_last([]), do: nil
 
-  def my_last([head | []]), do: head
+  def my_last([head]), do: head
 
   def my_last([_ | tail]), do: my_last(tail)
 end
